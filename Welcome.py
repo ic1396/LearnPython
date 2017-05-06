501bd4f33b2b6ba3b03f646da62a38ee903e8b1c
ic1396 <redsirius@foxmail.com>
2017-05-06 21:30:43 +0800

    《Python语言程序设计》 from ubuntu17.04

---
+++ Welcome.py
@@ -0,0 +1,5 @@
+#《Python语言程序设计》程序1-1
+# #Programe 1-1
+#Display two message
+print("Welcome to Python")
+print("Python is fun")
---
+++ WelcomeWithTreeMessage.py
@@ -0,0 +1,6 @@
+#《Python语言程序设计》程序1-2
+#Programe 1-2
+#Display three message
+print("Welcome to Python")
+print("Python is fun")
+print("Problem Driven")
