1ae4a36e3412a962ec2ccd03643d04e16ed57b1c
ic1396 <redsirius@foxmail.com>
2017-05-08 15:13:59 +0800

    《Python语言程序设计》程序清单１－测试Turtle模块  增加中文注释 书中没有的程序清单，是书中若干例子的集合

--- TestTurtleModule.py
+++ TestTurtleModule.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-#
-#Programe 1-
-#
-import turtle  #Import turtle module
+# 《Python语言程序设计》程序清单１－测试Turtle模块
+# Programed List 1-Test Turtle
+# Test Turtle in IDLE
+# 在IDLE或命令行中利用Turtle模块绘图的示例
+import turtle  # Import turtle module
+
 turtle.showturtle()
 turtle.write("Welcome to Python")
 turtle.forward(100)
@@ -14,13 +16,12 @@ turtle.color("blue")
 turtle.forward(100)
 turtle.right(45)
 turtle.forward(80)
-turtle.goto(0,50)
-turtle.goto(0,0)
-turtle.goto(0,50)
+turtle.goto(0, 50)
+turtle.goto(0, 0)
+turtle.goto(0, 50)
 turtle.penup()
-turtle.goto(50,-50)
+turtle.goto(50, -50)
 turtle.pendown()
 turtle.color("blue")
 turtle.circle(50)
 turtle.done()
-
