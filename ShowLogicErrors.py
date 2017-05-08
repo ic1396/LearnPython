d206a367e5fb1f02430a7a965fd41417ab1870f4
ic1396 <redsirius@foxmail.com>
2017-05-08 15:02:16 +0800

    《Python语言程序设计》程序清单１－４  增加中文注释

--- ShowLogicErrors.py
+++ ShowLogicErrors.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-print("Fahrenheit 35 is Celsius degree ")
+# 《Python语言程序设计》程序清单１－４
+# Programed List 1-4
+# Convert Fahrenheit to Celsius
+print("Fahrenheit 35 is Celsius degree ")  # 将温度３５华氏度转换为摄氏度
+# 逻辑错误的表达式
 print(5 / 9 * 35 - 32)
+# 逻辑正确的表达式
 print(5 / 9 * (35 - 32))
