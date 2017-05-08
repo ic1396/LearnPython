71f13fc0e7a1408c6d173927a981763a2dc53d15
ic1396 <redsirius@foxmail.com>
2017-05-09 00:11:09 +0800

    Chapter One Test Program from ubuntu17.04

---
+++ Chapter_One_Test_Programe.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+#
+#
+#
+
+import math
+
+# 1.1
+print("Welcome to Python")
+print("Welcome to Computer Science")
+print("Programming is fun")
+
+# 1.2
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+
+# 1.3
+print("FFFFFFF   U     U   NN    NN")
+print("FF        U     U   NNN   NN")
+print("FFFFFFF   U     U   NN N  NN")
+print("FF         U   U    NN  N NN")
+print("FF          UUU     NN   NNN")
+
+# 1.4
+print("a  a^2  a^3")
+a = 1
+print(a, end='')
+print("   ", end='')
+print(a ** 2, end='')
+print("    ", end='')
+print(a ** 3)
+
+a = 2
+print(a, end='')
+print("   ", end='')
+print(a ** 2, end='')
+print("    ", end='')
+print(a ** 3)
+
+a = 3
+print(a, end='')
+print("   ", end='')
+print(a ** 2, end='')
+print("    ", end='')
+print(a ** 3)
+
+a = 4
+print(a, end='')
+print("   ", end='')
+print(a ** 2, end='')
+print("   ", end='')
+print(a ** 3)
+
+# 1.5
+print((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5))
+
+# 1.6
+print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9)
+
+# 1.7
+print(4 * (1 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11))
+print(4 * (1 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13 - 1.0 / 15))
+print(4 * (1 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9 - 1 / 11))
+print(4 * (1 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9 - 1 / 11 + 1 / 13 - 1 / 15))
+
+# 1.8
+R = 5.5
+area = 5.5 ** 2 * math.pi
+perimeter = 2 * math.pi * R
+print("R=5.5")
+print("Area is ", end='')
+print(area)
+print("Perimeter is ", end='')
+print(perimeter)
+
+# 1.9
+width = 4.5
+height = 5.5
+area = width * height
+perimeter = 2 * (width + height)
+print("width = 4.5, height = 5.5")
+print("Area is ", end='')
+print(area)
+print("Perimeter is ", end='')
+print(perimeter)
+
+#1.10
+v = 14 / 1.6 / ((45+ 30 / 60)/60)
+print("V = ",end='')
+print(v,end='')
+print(" mail/h")
+
+#1.11
