e1f5a1bb5961a93541574730f1de72da74477e56
ic1396 <redsirius@foxmail.com>
2017-05-08 12:50:35 +0800

    《Python语言程序设计》程序清单１－３  增加中文注释

--- ComputerExpression.py
+++ ComputerExpression.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+# 《Python语言程序设计》程序清单１－３
+# Programed List 1-3
+# Computer expression
+print((10.5 + 2 * 3) / (45 - 3.5))
 
-#Programe 1-3
-#Computer expression
-print((10.5+2*3)/(45-3.5))
-
-#1.31
-print("3.5*4/2-2.5 is")
-print(3.5*4/2-2.5)
