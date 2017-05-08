881facf499559151f39fd50addad9deadf17c18f
ic1396 <redsirius@foxmail.com>
2017-05-08 15:07:54 +0800

    《Python语言程序设计》程序清单１－５  增加中文注释

--- OlympicSymbol.py
+++ OlympicSymbol.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-#
-#Programe 1-5
-#
+# 《Python语言程序设计》程序清单１－５
+# Programed List 1-5
+# Draw OlympicSymbol
 
-import turtle
+import turtle   # Import turtle module
 
 turtle.color("blue")
 turtle.penup()
-turtle.goto(-110,-25)
+turtle.goto(-110, -25)
 turtle.pendown()
 turtle.circle(45)
 
 turtle.color("black")
 turtle.penup()
-turtle.goto(0,-25)
+turtle.goto(0, -25)
 turtle.pendown()
 turtle.circle(45)
 
 turtle.color("red")
 turtle.penup()
-turtle.goto(110,-25)
+turtle.goto(110, -25)
 turtle.pendown()
 turtle.circle(45)
 
 turtle.color("yellow")
 turtle.penup()
-turtle.goto(-55,-75)
+turtle.goto(-55, -75)
 turtle.pendown()
 turtle.circle(45)
 
 turtle.color("green")
 turtle.penup()
-turtle.goto(55,-75)
+turtle.goto(55, -75)
 turtle.pendown()
 turtle.circle(45)
 
