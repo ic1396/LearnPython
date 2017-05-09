a591ca1d9f4cda6f7c7f88d6bc59693f7877a5d5
ic1396 <redsirius@foxmail.com>
2017-05-09 13:21:56 +0800

    《Python语言程序设计》程序清单２－１

---
+++ ComputeArea.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 《Python语言程序设计》程序清单２－１
+# Programed List 2-1
+# Compute Area of Circle
+
+# Assign a value to radius
+radius = 20    # radius is now 20
+
+# Compute area
+area = radius * radius * 3.14159
+
+# Display result
+print("The area for the circle of radius", radius, "is", area)
