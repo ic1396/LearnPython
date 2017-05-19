90ae583f6da7240ffdf20fadfa74fbc2573e28f6
ic1396 <redsirius@foxmail.com>
2017-05-19 14:39:20 +0800

    《Python语言程序设计》程序清单３－３

---
+++ DisplayUnicode.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 《Python语言程序设计》程序清单３－３
+# Programed List 3-3
+# Display Unicode on graphics
+
+import turtle
+
+turtle.write("\u6B22  \u8FCE  \u03b1  \u03b2  \u03b3")
+
+turtle.done()
