79db044ac36b60068b9bbc1b15e62001dd846e16
ic1396 <redsirius@foxmail.com>
2017-05-26 16:23:49 +0800

    《Python语言程序设计》程序清单３－检查点练习 完成至３．１６

--- Chapter_Third_Checkpoint.py
+++ Chapter_Third_Checkpoint.py
@@ -2,23 +2,70 @@
 # 《Python语言程序设计》程序清单３－检查点练习
 # Programed List 3-Checkpoint Programme
 # 第三章　检查点练习程序　３．１～３．３２
-
+import math
+'''
 # 3.1
+print("Checkpoint 3.1")
+print("（a） math.sqrt(4) ==",math.sqrt(4))
+print("（b） math.sin(2 * math.pi) ==",math.sin(2 * math.pi))
+print("（c） math.cos(2 * .math.pi) ==",math.cos(2 * math.pi))
+print("（d）min(2, 2, 1) ==",min(2, 2, 1))
+print("（e）math.log(math.e) ==",math.log(math.e))
+print("（f）math.exp(1) ==",math.exp(1))
+print("（g）max(2, 3, 4) ==",max(2, 3, 4))
+print("（h）abs(-2.5) ==",abs(-2.5))
+print("（i）math.ceil(-2.5) ==",math.ceil(-2.5))
+print("（j）math.floor(-2.5) ==",math.floor(-2.5))
+print("（k）round(3.5) ==",round(3.5))
+print("（l）round(-3.5) ==",round(-3.5))
+print("（m）math.fabs(2.5) ==",math.fabs(2.5))
+print("（n）math.ceil(2.5) ==",math.ceil(2.5))
+print("（o）math.floor(2.5) ==",math.floor(2.5))
+print("（p）round(-2.5) ==",round(-2.5))
+print("（q）round(2.6) ==",round(2.6))
+print("（r）round(math.fabs(-2.5)) ==",round(math.fabs(-2.5)))
 # 3.2
+print("Checkpoint 3.2")
 # 3.3
+print("Checkpoint 3.3")
+x = math.radians(47)
+print("math.radians(47) ==",x)
 # 3.4
+print("Checkpoint 3.4")
+x = math.degrees(math.pi / 7)
+print("math.degrees(math.pi / 7) ==",x)
 # 3.5
+print("Checkpoint 3.5")
+print("字符1、A、B、a和b的ＡＳＣＩＩ码依次为",ord('1'),ord('A'),ord('B'),ord('a'),ord('b'))
+print("ＡＳＣＩＩ码序号40、59、79、85和90所对应的字符依次为",chr(40),chr(59),chr(79),chr(85),chr(90))
 # 3.6
 # 3.7
 # 3.8
+print("Checkpoint 3.8")
+x = input("Enter a character: ")
+ch = chr(ord(x) + 3)
+print(ch)
 # 3.9
+print("Checkpoint 3.9")
+x = input("Enter a character: ")
+y = input("Enter a character: ")
+print(ord(y) - ord(x))
 # 3.10
+title = "Chapter " + 1
+print(title)
+title1 = "Chapter " + '1'
+print(title1)
 # 3.11
+sum = 2 + 3
+print(sum)
+s = '2' + '3'
+print(s)
 # 3.12
 # 3.13
 # 3.14
 # 3.15
 # 3.16
+'''
 # 3.17
 # 3.18
 # 3.19　
