e7c23130c7bd75d0bd505d362d15fbaf598c1c09
ic1396 <redsirius@foxmail.com>
2017-06-11 14:22:13 +0800

    《Python语言程序设计》程序清单４－６

---
+++ ComputeBMI.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 《Python语言程序设计》程序清单４－６
+# Programed List 4-6
+# Compute BMI by weight(pounds) and height(inches)
+
+# Prompt the user to enter weight in pounds
+weight = eval(input("Enter weight in pounds: "))
+
+# Prompt the user to enter height in inches
+height = eval(input("Enter height in inches: "))
+
+KILOGRAMS_PER_POUND = 0.45359237  # Constant
+METERS_PER_INCH = 0.0254  # Constant
+
+# Compute BMI
+weightInKilograms = weight * KILOGRAMS_PER_POUND
+heightInMeters = height * METERS_PER_INCH
+bmi = weightInKilograms / (heightInMeters * heightInMeters)
+
+# Display result
+print("BMI is", format(bmi, ".2f"))
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal")
+elif bmi < 30:
+    print("Overweight")
+else:
+    print("Obese")
