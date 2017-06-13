1e1a7e8447164a179f32292f2f8dea441447bcfb
ic1396 <redsirius@foxmail.com>
2017-06-13 15:33:55 +0800

    《Python语言程序设计》程序清单４－检查点练习 完成至 4.13

--- Chapter_Fourth_Checkpoint.py
+++ Chapter_Fourth_Checkpoint.py
@@ -3,19 +3,132 @@
 # Programed List 4-Checkpoint Programme
 # 第四章　检查点练习程序　４．１～４．３７
 
+import random
+'''
 # 4.1
 # 4.2
+i = int(True)
+j = int(False)
+b1 = bool(4)
+b2 = bool(0)
+print(i, j, b1, b2)
 # 4.3
+print(random.randrange(0, 20))
 # 4.4
+print(random.randrange(10, 20))
 # 4.5
+print(random.randint(10, 50))
 # 4.6
+print(random.randint(0, 1))
 # 4.7
+x = 0
+y = eval(input("请输入一个整数: "))
+if y > 0 :
+    x = 1
+print("x == " + str(x))
 # 4.8
+pay = eval(input("请输入当前支付额（整数）: "))
+score = eval(input("请输入评分（0~100间的数）: "))
+if score > 90.0 :
+    pay *= 1.03
+print("pay == " + str(pay))
 # 4.9
-# 4.0
+pay = eval(input("请输入当前支付额（整数）: "))
+score = eval(input("请输入评分（0~100间的数）: "))
+if score > 90.0 :
+    pay *= 1.03
+else :
+    pay *= 1.01
+print("pay == " + str(pay))
+# 4.10
+print("代码a：number == 30")
+number = 30
+if number % 2 == 0:
+    print (number, "is even.")
+print(number, "is odd.")
+print("代码a：number == 35")
+number = 35
+if number % 2 == 0:
+    print (number, "is even.")
+print(number, "is odd.")
+print("代码b：number == 30")
+number = 30
+if number % 2 == 0:
+    print(number, "is even.")
+else:
+    print(number, "is odd.")
+print("代码b：number == 35")
+number = 35
+if number % 2 == 0:
+    print(number, "is even.")
+else:
+    print(number, "is odd.")
 # 4.11
+print("x = 3而且 y = 2：")
+x = 3
+y = 2
+if x > 2:
+    if y > 2:
+        z = x + y
+        print("z is ", z)
+    else:
+        print("x is", x)
+print("x = 3而且 y = 4：")
+x = 3
+y = 4
+if x > 2:
+    if y > 2:
+        z = x + y
+        print("z is ", z)
+    else:
+        print("x is", x)
 # 4.12
+print("x = 2 而且 y = 4：")
+x = 2
+y = 4
+if x > 2:
+    if y > 2:
+        z = x + y
+        print("z is ", z)
+else:
+    print("x is", x)
+print("x = 3 而且 y = 2 ：")
+x = 3
+y = 2
+if x > 2:
+    if y > 2:
+        z = x + y
+        print("z is ", z)
+else:
+    print("x is", x)
+print("x = 3 而且 y = 3 ：")
+x = 3
+y = 3
+if x > 2:
+    if y > 2:
+        z = x + y
+        print("z is ", z)
+else:
+    print("x is", x)
 # 4.13
+score = eval(input("请输入评分（0~150间的数）: "))
+if score >= 60.0:
+    grade = 'D'
+    print("Grade(60) is ", grade)
+elif score >= 70.0:
+    grade = 'C'
+    print("Grade(70) is ", grade)
+elif score >= 80.0:
+    grade = 'B'
+    print("Grade(80) is ", grade)
+elif score >= 90.0:
+    grade = 'A'
+    print("Grade(90) is ", grade)
+else:
+    grade = 'F'
+    print("Grade(else) is ", grade)
+print("Final Grade is ", grade)
+'''
 # 4.14
 # 4.15
 # 4.16
