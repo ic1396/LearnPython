ba500e9b4be5c2ea34c3508118340ba77fc36482
ic1396 <redsirius@foxmail.com>
2017-10-30 12:28:11 +0800

    《Python语言程序设计》程序清单５－８ 创建

---
+++ GreatestCommonDivisor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 《Python语言程序设计》程序清单５－８
+# Programed List 5-8
+#
+
+#Prompt the user to enter two integers
+n1 = eval(input("Enter first integer: "))
+n2 = eval(input("Enter second integer: "))
+
+gcd = 1
+k = 2
+while k <= n1 and k <= n2:
+    if n1 % k == 0 and n2 % k == 0:
+        gcd = k
+    k += 1
+
+print("The greatest common divisor for",
+    n1, "and", n2, "is", gcd)
