9650623109b65f996910f984b9d414bb0c93189c
ic1396 <redsirius@foxmail.com>
2017-11-19 14:30:51 +0800

    新增： 《Python语言程序设计》程序清单６－检查点练习 《Python语言程序设计》程序清单６－编程题

---
+++ Chapter_sixth_Checkpoint.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# 《Python语言程序设计》程序清单６－检查点练习
+# Programed List 6-Checkpoint Programme
+# 第六章　检查点练习程序　６．１～６．２７
---
+++ Chapter_sixth_Test_Programe.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# 《Python语言程序设计》程序清单６－编程题
+# Programed List 6-Test Programme
+# 第六章　编程题　６．１～６．４８
