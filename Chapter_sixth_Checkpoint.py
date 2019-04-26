3f4bec7dc0a9f6e11d73a42c51c2f058b01af38b
ic1396 <ic1396@hotmail.com>
2019-04-26 22:14:56 +0800

    完成至6.10。

--- Chapter_sixth_Checkpoint.py
+++ Chapter_sixth_Checkpoint.py
@@ -10,19 +10,51 @@
 # 6.3
 
 # 6.4
-
+'''
 # 6.5
+def xFunction(x, y):
+    print(x + y)
+    return
 
+xFunction(2, 4)
+'''
 # 6.6
 
 # 6.7
-
+'''
 # 6.8
+def function1(n, m):
+function2(3, 4
+
+def function2(n):
+    if n >0 :
+        return 1
+    elif n == 0:
+        return 0
+    elif n < 0 :
+        return -1
+function1(2, 3)
 
 # 6.9
+def main():
+    print(min(5, 6))
+def min(n1, n2):
+    smallest = n1
+    if n2 < smallest:
+        smallest = n2
+
+main() # Call the main function
 
 # 6.10
+def main():
+    print(min(min(5, 6), (51, 6)))
+def min(n1, n2):
+    smallest = n1
+    if n2 < smallest:
+        smallest = n2
 
+main() # Call the main function
+'''
 # 6.11
 
 # 6.12
