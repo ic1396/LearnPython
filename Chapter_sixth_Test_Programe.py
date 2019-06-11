df26f5530fbde11d4d87287326dc92d9cef7ea27
“ic1396 <redsirius@foxmail.com>
2019-06-11 22:55:34 +0800

    《Python语言程序设计》程序清单６－编程题 完成至 6.8。

--- Chapter_sixth_Test_Programe.py
+++ Chapter_sixth_Test_Programe.py
@@ -89,13 +89,54 @@ def main():
     num1, num2, num3 = eval(input("请输入三个数："))
     displaySortedNumbers(num1, num2, num3)
 main():
-'''
 # 6.6
-# def displayPattern(n):
+def displayPattern(n):
+    for i in range(1, n + 1, 1):
+        j = 0
+        while j < n - i:
+            print("    ", end='')
+            j += 1
+        for k in range(i, 0, -1):
+            print(format(k, "4d"), end='')
+        print()
+def main():
+    n = eval(input("请输入一个正整数："))
+    displayPattern(n)
+main()
 # 6.7
-
+# 用指定的投资额度、年数和给定的利率计算未来投资价值
+def futureInvestmentValue(investmentAmount, monthlyInterestRate, years):
+    return investmentAmount * pow(1 + monthlyInterestRate, years * 12)
+def main():
+    investmentAmount = eval(input("请输入投资总额度："))
+    yearInterestRate = eval(input("请输入年利率："))
+    years = eval(input("请输入投资总年数："))
+    print("投资总额度：", investmentAmount)
+    print("年利率：", yearInterestRate)
+    print("投资总年数：", years)
+    print("  年  ", "  收益  ")
+    for i in range(1, years + 1, 1):
+        print(format(i, "3d"), "    ", format(futureInvestmentValue(investmentAmount, yearInterestRate / 12 / 100, i), "6.2f"))
+main()
 # 6.8
-
+# 摄氏度和华氏度间的互相转换
+# 摄氏度转华氏度
+def celsiusToFahrenheit(celsius):
+    return (9 / 5) * celsius + 32
+# 华氏度转摄氏度
+def fahrenheitToCelsius(fahrenheit):
+    return (5 / 9) * (fahrenheit - 32)
+def main():
+    print("______________________________________________________")
+    print("  Celsius  |  Fahrenheit  ||  Fahrenheit  |  Celsius  ")
+    print("------------------------------------------------------")
+    cel = 40.0
+    fah = 120.0
+    for i in range(0, 10, 1):
+        print("  ", format(cel - i, "5.1f"), "  |  ", format(celsiusToFahrenheit(cel - i), "8.1f"),"  ||  ",
+              format(fah - i * 10, "8.1f"), "  |  ", format(fahrenheitToCelsius(fah - i * 10), "4.2f"))
+main()
+'''
 # 6.9
 
 # 6.10
