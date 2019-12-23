6cabe910b1b8a7badeb92e466244c3fc64e7615f
ic1396 <redsirius@foxmail.com>
2019-12-23 20:49:02 +0800

    《Python语言程序设计》程序清单８－１

---
+++ CheckPalindrome.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# 《Python语言程序设计》程序清单８－１
+# Programed List 7-8
+# 示例：校验回文串
+
+def main():
+    # Prompt the user to enter a string
+    s = input("Enter a string:")
+
+    if isPalindrome(s):
+        print(s, "is a palindrome")
+    else:
+        print(s, "is not a palindrome")
+
+# Check if a string is a palindrome
+def isPalindrome(s):
+    # The index of the first character in the string
+    low = 0
+
+    # The index of the last character in the string
+    high = len(s) - 1
+
+    while low < high:
+        if s[low] != s[high]:
+            return False # Not a palindrome
+        low += 1
+        high -= 1
+
+    return True # The string is a palindrome
+
+main()    # Call the main function
