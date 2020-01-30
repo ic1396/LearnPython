2bd4a1dfdc241b8793c604242f0f473f50cb7bbc
ic1396 <redsirius@foxmail.com>
2020-01-30 10:15:18 +0800

    《Python语言程序设计》程序清单８－３

---
+++ TestRationalClass.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 《Python语言程序设计》程序清单８－３
+# Programed List 8-3
+# 示例：测试 Rational 类。
+
+import Rational
+
+# Create and initialize two rational numbers r1 and r2
+r1 = Rational.Rational(4, 2)
+r2 = Rational.Rational(2, 3)
+
+# Display results
+print(r1, "+", r2, "=", r1 + r2)
+print(r1, "-", r2, "=", r1 - r2)
+print(r1, "*", r2, "=", r1 * r2)
+print(r1, "/", r2, "=", r1 / r2)
+
+print(r1, ">", r2, "is", r1 > r2)
+print(r1, ">=", r2, "is", r1 >= r2)
+print(r1, "<", r2, "is", r1 < r2)
+print(r1, "<=", r2, "is", r1 <= r2)
+print(r1, "==", r2, "is", r1 == r2)
+print(r1, "!=", r2, "is", r1 != r2)
+
+print("int(r2) is", int(r2))
+print("float(r2) is", float(r2))
+
+print("r2[0] is", r2[0])
+print("r2[1] is", r2[1])
